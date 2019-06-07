51ff46e4bdb96de869f9ccba2d6db7ba016a22ce
meetabug <1272526724@qq.com>
2019-06-07 15:16:20 +0800

    视图共享数据

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Carbon::setLocale('zh');
+        \View::share('channels',\App\Channel::all());
     }
 
     /**
